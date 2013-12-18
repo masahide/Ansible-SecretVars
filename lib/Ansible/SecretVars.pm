6dd74e10587aebbbd2390f95289f4ece39edc4ec
masahide <masahide.y@gmail.com>
2013-12-18 23:05:54 +0900

    initial commit

---
+++ lib/Ansible/SecretVars.pm
@@ -0,0 +1,32 @@
+package Ansible::SecretVars;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Ansible::SecretVars -
+
+=head1 SYNOPSIS
+
+  use Ansible::SecretVars;
+
+=head1 DESCRIPTION
+
+Ansible::SecretVars is
+
+=head1 AUTHOR
+
+YAMASAKI Masahide E<lt>masahide.y@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
